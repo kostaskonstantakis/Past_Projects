b55a82fe7eb582690d57c07c1839f501684e59b6
Kostas Konstantakis <csd3219@csd.uoc.gr>
2020-11-27 17:02:58 +0200

    Add files via upload

---
+++ e-learning_system/src/main/java/Utilities/SecurePassword.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author gussl
+ */
+public class SecurePassword 
+{
+    public static String getSecurePassword(String password)
+    {
+        String passwordToHash = password;
+        String generatedPassword = null;
+        
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        
+        return generatedPassword;
+    }
+}
